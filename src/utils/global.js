aee983c38972770138c4b415bd2284f3da3fbdf9
Yusuf Fazeri <yusuffazeri00@gmail.com>
2024-06-09 15:33:46 +0700

    add estimation reading time

--- src/utils/global.js
+++ src/utils/global.js
@@ -1,3 +1,6 @@
+import { toString } from 'mdast-util-to-string';
+import getReadingTime from 'reading-time';
+
 export const getFormattedDate = (date) =>
 	date
 		? new Date(date).toLocaleDateString('en-us', {
@@ -6,3 +9,14 @@ export const getFormattedDate = (date) =>
 				day: 'numeric',
 		  })
 		: '';
+
+// https://docs.astro.build/en/recipes/reading-time/
+export function remarkReadingTime() {
+	return function (tree, { data }) {
+		const textOnPage = toString(tree);
+		const readingTime = getReadingTime(textOnPage);
+		// readingTime.text will give us minutes read as a friendly string,
+		// i.e. "3 min read"
+		data.astro.frontmatter.minutesRead = readingTime.text;
+	};
+}
